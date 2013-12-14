2e041897fa0997e586898490655379b40cd56e1b
Hugh Winkler <hwinkler@drillinginfo.com>
2013-12-14 06:51:45 -0600

    Add a small facade over curand

--- rnd.cu
+++ rnd.cu
@@ -1,22 +1,12 @@
 #include <cuda.h>
 #include <curand_kernel.h>
 
-#define CUDA_CALL(x) do { if((x) != cudaSuccess) { \
-    printf("Error at %s:%d\n",__FILE__,__LINE__); \
-    return EXIT_FAILURE;}} while(0)
-
-__device__ double mt_drand(curandState* globalState) {
-  return 0.34;
-  //int ind = threadIdx.x;
-  //curandState localState = globalState[ind];
-  //return curand_uniform( &localState );
-}
-__device__  void mt_seed(){
-
+__device__ double rnd(curandState* state) {
+  return curand_uniform( state );
 }
 
-__global__ void setup_rnd_kernel ( curandState * state, unsigned long seed )
-{
-    int id = threadIdx.x;
-    curand_init ( seed, id, 0, &state[id] );
+__device__ void rndSeed ( curandState * state){
+  unsigned int seed = (unsigned int) clock64();
+  int id = threadIdx.x + blockIdx.x * blockDim.x;
+  curand_init ( seed, id, 0, &state[id] );
 } 
---
+++ rnd.h
@@ -0,0 +1,5 @@
+#include <cuda.h>
+#include <curand_kernel.h>
+
+__device__ double rnd(curandState* globalState);
+__device__ void rndSeed ( curandState * state, unsigned long seed );
